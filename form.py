8aae8e127a4f18b9b12659ebb7e8084874e30d3e
TonyHsu <sunprince12014@gmail.com>
2015-06-06 09:07:43 +0800

    refresh

--- form.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-NBEST = 2
-with open('test.txt','r') as fin,open('test.txt_'+ str(NBEST) , 'w') as fout:
-    for line in fin:
-        line = line.rstrip().split(' ')
-        lineNo = int(line[0]) //NBEST
-        content = ' '.join(line[1:])
-        fout.write("{0} {1}\n".format(lineNo,content)) 
